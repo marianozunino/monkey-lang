7eea314b92bbe5b519d7094f81857cdc3ab3f6bb
Mariano Z <marianoz@posteo.net>
2023-06-05 16:49:04 -0300

    feat: add simple repl

---
+++ src/repl.ts
@@ -0,0 +1,27 @@
+import * as readline from "node:readline/promises";
+
+import { stdin as input, stdout as output } from "node:process";
+import { Lexer } from "./lexer";
+import { TokenType } from "./token";
+
+const rl = readline.createInterface({ input, output });
+
+async function main() {
+  rl.write("Welcome to the Monkey REPL. Type exit to exit\n");
+  while (true) {
+    const input = await rl.question(">> ");
+
+    if (input === "exit") {
+      rl.close();
+      return;
+    }
+    const l = new Lexer(input);
+    let token = l.nextToken();
+    while (token.type != TokenType.EOF) {
+      output.write(`${JSON.stringify(token)}\n`);
+      token = l.nextToken();
+    }
+  }
+}
+
+main();
