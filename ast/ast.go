74a8fa9ad8701a54ec150c67aade72355c5c16d1
Mariano Z. <marianoz@posteo.net>
2024-03-08 13:38:39 -0300

    feat: add string method for easier debugging

--- ast/ast.go
+++ ast/ast.go
@@ -4,6 +4,7 @@ import "github.com/marianozunino/monkey-lang/token"
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -28,10 +29,12 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-type LetStatement struct {
-	Token token.Token // the LET token
-	Name  *Identifier
-	Value Expression
+func (p *Program) String() string {
+	var out string
+	for _, s := range p.Statements {
+		out += s.String()
+	}
+	return out
 }
 
 type Identifier struct {
@@ -39,8 +42,27 @@ type Identifier struct {
 	Value string
 }
 
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
+
+type LetStatement struct {
+	Token token.Token // the LET token
+	Name  *Identifier
+	Value Expression
+}
+
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out string
+	out += ls.TokenLiteral() + " " + ls.Name.String() + " = "
+	if ls.Value != nil {
+		out += ls.Value.String()
+	}
+	out += ";"
+	return out
+}
 
 type ReturnStatement struct {
 	Token       token.Token // the RETURN token
@@ -49,3 +71,26 @@ type ReturnStatement struct {
 
 func (ls *ReturnStatement) statementNode()       {}
 func (ls *ReturnStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *ReturnStatement) String() string {
+	var out string
+	out += ls.TokenLiteral() + " "
+	if ls.ReturnValue != nil {
+		out += ls.ReturnValue.String()
+	}
+	out += ";"
+	return out
+}
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -5,7 +5,29 @@ import (
 
 	"github.com/marianozunino/monkey-lang/ast"
 	"github.com/marianozunino/monkey-lang/lexer"
+	"github.com/marianozunino/monkey-lang/token"
 )
+
+func TestString(t *testing.T) {
+	program := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
 
 func TestLetStatements(t *testing.T) {
 	input := `
